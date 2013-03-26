5986966d6b345bbe3ba39820e319c4f9660887a9
joshdforbes <jdforbes82@gmail.com>
2013-03-25 21:07:31 -0400

    Reworked the quiz application from the ground up.
    quiz is now contained in a module, fixing some of the problems I was having with private and public variables. Code has been further broken up into smaller functions/methods hopefully making it easier to update in the future.
    
    The ideal of the current excercise is to build this with no jquery so I can practice my understanding of vanilla javascript, next up is learning jquery and refactoring this code for better browser compatability.

--- main.js
+++ main.js
@@ -1,126 +1,226 @@
-questionsArray = [
+questionArray = [
     {   question: "Who is the blah blah blah",
         choices: ["That Guy1", "That Guy2"],
-        answer: "That Guy1"
+        answer: "0"
     },
     {   question: "Who is the yayaya",
         choices: ["This Girl3", "This Girl2", "That Girl3"],
-        answer: "That Guy1"
+        answer: "2"
     },
     {   question: "Who is the ufdasfa",
         choices: ["That Guy1", "That Guy2"],
-        answer: "That Guy1"
+        answer: "1"
     }
 ];
 
-function Quiz(questionsArray){
-    this.questionsArray = questionsArray;
-    this.totalQuestions = this.questionsArray.length;
-    this.counter = 0;
 
-    var userAnswers = [];
-    var userScore = [];
+var quizModule = function() {
+    var quizElement = document.querySelector("#quiz");
+    //this.questionArray = questionArray;
+    var questionObjectArray = [];
+    var numberOfQuestions = questionArray.length;
+    var questionCounter = 0;
+    var currentQuestionCounter = 0;
+    var fragment = document.createDocumentFragment();
 
-    this.calculateScore = function() {
-        for (var i in userAnswers){
-            if (userAnswers[i] === this.questionsArray[i]["answer"]){
-                this.incrementUserScore(i);
+    function Question(question, choices, correctAnswer){
+        this.question = question;
+        this.choices = choices;
+        this.correctAnswer = correctAnswer;
+        this.userAnswer = "";
+    }
+
+    Question.prototype = {
+        constructor: Question,
+
+        getCorrectAnswer: function() {
+            return this.correctAnswer;
+        },
+        getUserAnswer: function() {
+            return this.userAnswer;
+        },
+        questionHTML: function(){
+            var questionElement = "<h1>" +
+                this.question + "</h1><ul>";
+            for (var i = 0; i < this.choices.length; i++){
+                questionElement += "<li><input type='radio' name='choice' value='" + [i] + "'>" + this.choices[i] + "</li>";
             }
+            questionElement += "</ul>";
+            return questionElement;
+        },
+        questionToDivElement: function() {
+            var child = this.questionHTML();
+            var div = document.createElement("div");
+            div.innerHTML = child;
+            div.id = "question" + questionCounter++;
+            div.className = "question";
+            return div;
         }
     }
 
-    this.getUserScore = function() {
-        return "You answered " + userScore.length + " questions correctly out of a possible " + this.questionsArray.length + ".";
+
+    function createQuestionObject(){
+        var question = new Question(questionArray[0]["question"], questionArray[0]["choices"], questionArray[0]["answer"]);
+        //store our question object in array for later reference
+        questionObjectArray.push(question);
+        //remove the question from the original array
+        questionArray.shift();
+
+        //send the question object to the documentfragment for storage
+        fragment.appendChild(question.questionToDivElement());
     }
 
-    this.incrementUserScore = function(correctAnswerNumber) {
-        userScore.push(correctAnswerNumber);
+    function createQuestions(){
+        for (var i = 0; i < numberOfQuestions; i++){
+            createQuestionObject();
+        }
     }
 
-    this.getUserAnswers = function() {
-        return userAnswers;
+    function getCurrentQuestion(){
+        var quiz = document.querySelector("#quiz");
+        var currentQuestion = fragment.querySelector("#question" + currentQuestionCounter);
+        console.log(currentQuestion);
+        quiz.insertBefore(currentQuestion, document.querySelector("#quizPrevious"));
     }
-    this.setUserAnswers = function(value) {
-        userAnswers.push(value);
+
+    function removeCurrentQuestion(){
+        var currentQuestion = document.querySelector("#question" + currentQuestionCounter);
+        fragment.appendChild(currentQuestion);
     }
-}
 
+    function createButtons(){
+        var quiz = document.querySelector("#quiz");
 
-
-Quiz.prototype = {
-    constructor: Quiz,
-
-    displayQuestion: function(e, counter) {
-        var currentQuestion = this.questionsArray[counter]["question"];
-        e = document.getElementById(e);
-        e.innerHTML = "Question " + (counter + 1) + ": " + currentQuestion + "<br>"
-    },
-
-    createInput: function(type, name, value){
-        this.type = type;
-        this.name = name;
-        this.value = value;
-
-        var createdInput = document.createElement('input');
-        createdInput.setAttribute('type', this.type);
-        createdInput.setAttribute('name', this.name);
-        createdInput.setAttribute('value', this.value);
-        return createdInput;
-    },
-
-    displayChoices: function(e, counter) {
-        e = document.getElementById(e);
-        var currentChoices = this.questionsArray[counter]["choices"];
-        e.innerHTML = "";
-
-        for (var choice in currentChoices) {
-            radio = this.createInput('radio', 'answerChoice', currentChoices[choice]);
-            e.appendChild(radio);
-            e.innerHTML += currentChoices[choice] + "<br>";
+        var nextButton = document.createElement("input");
+        nextButton.id = "quizNext";
+        nextButton.type = "button";
+        nextButton.value = "Next";
+        nextButton.onclick = function() {
+           nextQuestion();
         }
 
-    },
+        var previousButton = document.createElement("input");
+        previousButton.id = "quizPrevious";
+        previousButton.type = "button";
+        previousButton.value = "Previous";
+        previousButton.onclick = function() {
+            previousQuestion();
+        }
+        previousButton.style.opacity = .4;
 
-    getAnswer: function() {
-        //don't like this, figure out a better way to pass the radio buttons from createInput or displayChoices
-        var choices = document.getElementsByTagName("input");
+        quiz.appendChild(previousButton);
+        quiz.appendChild(nextButton);
 
-        for (var i in choices){
-            if (choices[i].checked){
-                this.setUserAnswers(choices[i].value);
-                return true;
+    }
+
+    function nextQuestion() {
+        if (getAnswer()){
+            getAnswer();
+        }
+        if (currentQuestionCounter < numberOfQuestions - 1 ){
+            removeCurrentQuestion();
+            currentQuestionCounter++;
+            getCurrentQuestion();
+        }
+        if (currentQuestionCounter === numberOfQuestions - 1){
+            submitButtonToggle();
+        }
+
+        //if next is pushed then no longer on first question, full opacity on previous button
+        var button = document.querySelector("#quizPrevious");
+        button.style.opacity = 1;
+
+    }
+
+    function previousQuestion(){
+        if (getAnswer()){
+            getAnswer();
+        }
+
+        //change submit button back to next button if not at end of test
+        if (currentQuestionCounter === numberOfQuestions -1){
+            submitButtonToggle();
+        }
+
+        //gray out previous button if unusable
+        var previousButton = document.querySelector("#quizPrevious");
+        if (currentQuestionCounter === 1){
+            previousButton.style.opacity = .4;
+        }
+
+        //display previous question
+        if (currentQuestionCounter > 0){
+            removeCurrentQuestion();
+            currentQuestionCounter--;
+            getCurrentQuestion();
+        }
+    }
+
+    function submitButtonToggle() {
+        var button = document.querySelector("#quizNext");
+        if (button['value'] === 'Next'){
+            button['value'] = 'Submit';
+            button.onclick = function() {
+                submit();
             }
-        }
-    },
-
-    nextQuestion: function() {
-        if (!this.getAnswer()){
-            alert("please select an answer");
-            return;
-        }
-        if (this.counter < (this.totalQuestions - 1)){
-            this.counter++;
-            this.displayQuiz();
         } else {
-            this.calculateScore();
-            alert(this.getUserScore());
+            button['value'] = 'Next';
+            button.onclick = function(){
+                nextQuestion();
+            }
         }
-    },
+    }
 
-    previousQuestion: function() {
-        if (this.counter > 0) this.counter--;
-        this.displayQuiz();
-    },
-
-    displayQuiz: function() {
-        this.displayQuestion("questions", this.counter);
-        this.displayChoices("choices", this.counter);
+    function submit() {
+        var answerResults = calculateScore();
+        var string = "<div class='results'><h1>Results:</h1><ul>";
+        for (var i = 0; i < answerResults.length ; i ++){
+            string += "<li>Question " + (i + 1) + ": " + answerResults[i] + "</li>";
+        }
+        string += "</ul></div>"
+        document.querySelector("#quiz").innerHTML = string;
 
     }
 
-}
 
-var quiz = new Quiz(questionsArray);
+    function getAnswer(){
+        var radioButtons = document.querySelectorAll("input[type='radio']");
+        for (i in radioButtons){
+           if (radioButtons[i].checked){
+               questionObjectArray[currentQuestionCounter].userAnswer = radioButtons[i].value;
+               return true;
+           }
+        }
+    }
+
+    function calculateScore(){
+        var answerResults = [];
+        for (var i = 0; i < questionObjectArray.length; i++){
+            if (questionObjectArray[i]['correctAnswer'] === questionObjectArray[i]['userAnswer']){
+                answerResults[i] = 'Correct';
+            } else {
+                answerResults[i] = 'Incorrect';
+            }
+        }
+        return answerResults;
+    }
+
+    function startQuiz(){
+        createQuestions();
+        getCurrentQuestion();
+        createButtons();
+    }
+
+    return {
+        startQuiz: startQuiz,
+        nextQuestion: nextQuestion,
+        previousQuestion: previousQuestion,
+        getAnswer: getAnswer
+
+     };
+}();
+
 window.onload = function() {
-    quiz.displayQuiz();
+    quizModule.startQuiz();
+
 }
